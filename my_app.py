f75e339fa7ab332ef3e7710186ef9913af0491a9
Tamara Zdorova <toma-kukina@yandex.ru>
2017-06-21 00:02:17 +0400

    commit

---
+++ my_app.py
@@ -0,0 +1,105 @@
+import re
+from collections import defaultdict
+from random import uniform
+import conf
+import telebot
+import flask
+
+r_alphabet = re.compile(u'[а-яА-Я0-9-]+|[.,:;?!]+')
+bot = telebot.TeleBot(conf.TOKEN, threaded=False)
+WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
+app = flask.Flask(__name__)
+
+def gen_lines(corpus):
+    data = open(corpus)
+    for line in data: #192842
+        yield line.lower()
+
+
+def gen_tokens(lines):
+    for line in lines:
+        for token in r_alphabet.findall(line):
+            yield token
+
+def gen_trigrams(tokens):
+    t0, t1 = '$', '$'
+    for t2 in tokens:
+        yield t0, t1, t2
+        if t2 in '.!?':
+            yield t1, t2, '$'
+            yield t2, '$','$'
+            t0, t1 = '$', '$'
+        else:
+            t0, t1 = t1, t2
+
+def train(corpus):
+    lines = gen_lines(corpus)
+    tokens = gen_tokens(lines)
+    trigrams = gen_trigrams(tokens)
+
+    bi, tri = defaultdict(lambda: 0.0), defaultdict(lambda: 0.0)
+
+    for t0, t1, t2 in trigrams:
+        bi[t0, t1] += 1
+        tri[t0, t1, t2] += 1
+
+    model = {}
+    for (t0, t1, t2), freq in iter(tri.items()):
+        if (t0, t1) in model:
+            model[t0, t1].append((t2, freq/bi[t0, t1]))
+        else:
+            model[t0, t1] = [(t2, freq/bi[t0, t1])]
+    return model
+
+def generate_sentence(model):
+    phrase = ''
+    t0, t1 = '$', '$'
+    while 1:
+        t0, t1 = t1, unirand(model[t0, t1]) #unirand
+        if t1 == '$': break
+        if t1 in ('.!?,;:') or t0 == '$':
+            phrase += t1
+        else:
+            phrase += ' ' + t1
+    return phrase.capitalize()
+
+def unirand(seq):
+    sum_, freq_ = 0, 0
+    for item, freq in seq:
+        sum_ += freq
+    rnd = uniform(0, sum_)
+    for token, freq in seq:
+        freq_ += freq
+        if rnd < freq_:
+            return token
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Здравствуйте! Сейчас я буду с Вами разговаривать.')
+
+@bot.message_handler(func=lambda m: True)
+def send_phrase(message):
+    model = train('1.txt')
+    phrase = generate_sentence(model)
+    bot.send_message(message.chat.id, phrase)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'ok'
+
+#if __name__ == '__main__':
+#   bot.polling(none_stop=True)
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
